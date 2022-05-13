fe8aeae805da16ebe9b087849e2c3abe938ee7b5
Ayush Gupta <guptaayush06348@gmail.com>
2022-05-13 11:31:18 +0530

    markup complete

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
-})
+});
+
+app.get("/categories", (req, res) => {
+  const categories = [{
+      name: "fish",
+      image: "/public/images/fish.jpg"
+    },
+    {
+      name: "crustaceans",
+      image: "/public/images/fish.jpg"
+    },
+    {
+      name: "exotic",
+      image: "/public/images/fish.jpg"
+    }
+  ];
+  res.json(categories)
+});
 
 app.listen(port, function() {
   console.log(`Server is running on port ${port}`);
--- jsx/index.jsx
+++ jsx/index.jsx
@@ -1,2 +1,3 @@
 import "/node_modules/react/umd/react.production.min.js";
+import "/node_modules/react-dom/umd/react-dom.production.min.js";
 console.log(React);
